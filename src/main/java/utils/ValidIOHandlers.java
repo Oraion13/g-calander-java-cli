602c95b53c6999e4ad078bd5117a240bfa2d3457
Oraion13 <65845210+Oraion13@users.noreply.github.com>
2022-09-18 11:46:10 +0530

    valid IO handlers to get user input
    Choice, Date, Message

--- src/main/java/utils/ValidIOHandlers.java
+++ src/main/java/utils/ValidIOHandlers.java
@@ -4,13 +4,12 @@ import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.time.format.ResolverStyle;
 import java.util.Locale;
-import java.util.Scanner;
 
 public class ValidIOHandlers {
     /**
      * Get input choice
      * 
-     * @param sc Scanner object to get input
+     * @param message message to display
      * @return an integer choice
      * @throws IOException
      */
@@ -18,40 +17,89 @@ public class ValidIOHandlers {
         while (true) {
             try {
                 System.out.print(message);
-
                 int choice = Integer.parseInt(System.console().readLine());
 
                 // if correct input is given, return the input
                 if (choice >= 0)
                     return choice;
             } catch (NumberFormatException e) {
-                System.out.println("Enter a valid input (Number)!");
+                System.out.println("Enter a valid input [Number]!");
             }
 
         }
+    }
 
+    /**
+     * Get valid y / n choice
+     * 
+     * @param message message to display
+     * @return a boolean true / false
+     */
+    public static boolean getYorN(String message) {
+        while (true) {
+            System.out.print(message);
+            String choice = System.console().readLine().toLowerCase();
+
+            // y - true
+            if (choice.equals("y")) {
+                return true;
+            }
+
+            // n - false
+            if (choice.equals("n")) {
+                return false;
+            }
+
+            System.out.println("Enter a valid input [Y/n]!");
+        }
     }
 
     /**
      * Get valid date and return
      * 
-     * @param sc      Scanner object to get input
-     * @param message Input message
+     * @param message message for user
      * @return a valid dateTime
      */
-    public static String getDateTime(Scanner sc, String message) {
+    public static String getDate(String message, int confirm) {
         while (true) {
-
             System.out.print(message);
-            String dateStr = sc.next();
+            String dateStr = System.console().readLine();
 
-            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("uuuu-MM-dd", Locale.US)
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("uuuu-MM-dd", Locale.getDefault())
                     .withResolverStyle(ResolverStyle.STRICT);
             DateValidatorUsingDateTimeFormatter validator = new DateValidatorUsingDateTimeFormatter(dateFormatter);
 
-            if (validator.isValid(dateStr))
-                return dateStr;
+            // is a valid date
+            if (confirm == 1) {
+                if (validator.isValid(dateStr)) {
+                    return dateStr;
+                }
+            } else {
+                if (validator.isValid(dateStr) || dateStr.equals("0")) {
+                    return dateStr;
+                }
+            }
 
+            System.out.println("Enter a valid date [YYYY-MM-DD]!");
+        }
+    }
+
+    /**
+     * Get a valid message
+     * 
+     * @param message message for user
+     * @return
+     */
+    public static String getString(String message) {
+        while (true) {
+            System.out.print(message);
+            String str = System.console().readLine();
+
+            if (!str.equals(null)) {
+                return str;
+            }
+
+            System.out.println("Enter a valid message!");
         }
     }
 
