96e34ae714abf2faa151a79521356523b974f095
Oraion13 <65845210+Oraion13@users.noreply.github.com>
2022-09-18 12:07:50 +0530

    cleaned up Delete events file

--- src/main/java/controllers/DeleteEvents.java
+++ src/main/java/controllers/DeleteEvents.java
@@ -1,11 +1,11 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import com.google.api.services.calendar.model.Event;
+
+import utils.ValidIOHandlers;
 
 public class DeleteEvents {
     EventsManagement eventsManagement = null;
@@ -21,45 +21,34 @@ public class DeleteEvents {
         System.out.println("\t\tDelete Events");
         System.out.println("------------------------------------------\n");
         while (true) {
-            System.out.println("Get events from - to...\nEnter '0' to exit...");
-            System.out.print("Enter the start date(YYYY-MM-DD / 0): ");
+            System.out.println("Get events From - To...\nEnter '0' to exit...\n");
 
-            String start = System.console().readLine();
-            if (start.equals("0"))
+            // get events start and ending date
+            String from = ValidIOHandlers.getDate("Enter the start date [YYYY-MM-DD / 0]: ", 0);
+            if (from.equals("0"))
                 break;
-
-            System.out.print("Enter the date 'To', '0' for no ending date(YYYY-MM-DD / 0): ");
-            String end = System.console().readLine();
+            String to = ValidIOHandlers.getDate("Enter the date 'To', '0' for no ending date [YYYY-MM-DD / 0]: ", 0);
 
             // events limit
+            int limit = ValidIOHandlers.getChoice("Enter the number of events to get [Number]: ");
 
-            System.out.print("Enter the number of events to get(Number): ");
-            int limit = Integer.parseInt(System.console().readLine());
-
-            deleteEvents(start, end, limit);
+            deleteEvents(from, to, limit);
         }
     }
 
-    // display events to be deleted
-    private void deleteEvents(String start, String end, int limit) throws IOException {
-        Map<Integer, Event> toDelete = new HashMap<>();
-        int counter = 0;
-
-        // get a list of events
-        List<Event> events = eventsManagement.getEventsBetween(start, end, 10);
-
-        // put all events with an associated ID to delete
-        for (Event event : events) {
-            toDelete.put(++counter, event);
-        }
-
+    /**
+     * Display and delete events
+     * 
+     * @param from  Event starting date
+     * @param to    Event ending date
+     * @param limit Events limit
+     * @throws IOException
+     */
+    private void deleteEvents(String from, String to, int limit) throws IOException {
+        Map<Integer, Event> toDelete = eventsManagement.getEventsInMap(from, to, limit);
         // display the events
-        System.out.println("Delete ID   :   Event\n-----------------------------------");
-        for (Map.Entry<Integer, Event> event : toDelete.entrySet()) {
-            System.out.println(
-                    event.getKey() + " : " + event.getValue().getSummary() + " || "
-                            + event.getValue().getStart().getDateTime());
-        }
+        System.out.println("Delete ID :   Event\n-----------------------------------");
+        eventsManagement.printMapEvents(toDelete);
 
         System.out.print("Enter a Delete ID to delete an event (Number / 0): ");
         int deleteID = Integer.parseInt(System.console().readLine());
